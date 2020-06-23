6fb991a5f36cf872771fbf5168b01f601520be82
Pavel Baksheev <sxeiter@mail.ru>
2020-06-23 12:14:12 +0500

    upd HW 12

--- script/script.js
+++ script/script.js
@@ -1,17 +1,28 @@
 'use strict';
 
 const todoControl = document.querySelector('.todo-control'),
-    headerInput = document.querySelector('.header-input'), 
+    headerInput = document.querySelector('.header-input'), //строка ввода
     todoList = document.querySelector('.todo-list'),
-    todoCompleted = document.querySelector('.todo-completed');
+    todoCompleted = document.querySelector('.todo-completed'), // выполненные дела
+    todoRemove = document.querySelector('.todo-remove'); //кнопка удаления
     
-const todoData = [];
+let todoData = [];
+
+const parseJSON = function(){
+    let storageJSON = localStorage.getItem('json');
+    if(storageJSON === null){
+        return;
+    } else {
+        todoData = JSON.parse(storageJSON);
+    }
+};
+parseJSON();
 
 const render = function(){
     todoList.textContent = '';
     todoCompleted.textContent = '';
 
-    todoData.forEach(function(item){
+    todoData.forEach(function(item, index){
         const li = document.createElement('li');
         li.classList.add('todo-item');
 
@@ -27,26 +38,39 @@ const render = function(){
             todoList.append(li);
         }
 
-        const btnTodoCompleted = li.querySelector('.todo-complete');
+    const btnTodoCompleted = li.querySelector('.todo-complete');
+    const btnTodoRemove = li.querySelector('.todo-remove');
 
         btnTodoCompleted.addEventListener('click', function(){
             item.completed = !item.completed;
             render();
         });
+        btnTodoRemove.addEventListener('click', function(){
+            todoData.splice(index, 1);
+            render();
+        });
     });
+        
+    let json = JSON.stringify(todoData);
+    localStorage.setItem('json', json);
 };
+
 
 todoControl.addEventListener('submit', function(event){
     event.preventDefault();
-
-    const newTodo = {
-        value: headerInput.value,
-        completed: false
-    };
-
-    todoData.push(newTodo);
-
-    render();
+    if(headerInput.value !== ''){
+        const newTodo = {
+            value: headerInput.value,
+            completed: false
+        };
+        todoData.push(newTodo);
+        headerInput.value = '';
+        render();
+    }    
 });
 
 render();
+
+//if(!.value) return; //нужно подставить переменную, пустая строка не будет записываться
+//if(todoData.lenght === 0) todo.innerHTML = ''; // вывод пустой строки если нет ничего в записях
+//значение.value = ''; //после записи в список дел строка становится пустой
